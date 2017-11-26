63e8712635745a3e10bafa25a646555ac2dbc354
adnan119 <adnanpen119@gmail.com>
2017-11-26 16:30:23 +0530

    loop through the programm to run multiple times

--- Take_a_Break.py
+++ Take_a_Break.py
@@ -1,6 +1,13 @@
 import webbrowser
 import time
 
-time_to_wait=10
-time.sleep(time_to_wait)
-webbrowser.open("https://docs.python.org/2/library/webbrowser.html")
+
+total_breaks=3
+break_count=0
+
+while break_count < total_breaks:
+
+    time_to_wait=10
+    time.sleep(time_to_wait)
+    webbrowser.open("https://docs.python.org/2/library/webbrowser.html")
+    break_count = break_count+1
