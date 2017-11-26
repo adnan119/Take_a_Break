f66a9401e3f20bf8a0bde5a31709cf2719997c25
adnan119 <adnanpen119@gmail.com>
2017-11-26 16:41:00 +0530

    add url as a different variable

--- Take_a_Break.py
+++ Take_a_Break.py
@@ -5,9 +5,10 @@ import time
 total_breaks=3
 break_count=0
 time_to_wait=10
+url="https://www.youtube.com/"
 
 print("this program started on " + time.ctime())
 while break_count < total_breaks:
     time.sleep(time_to_wait)
-    webbrowser.open("https://docs.python.org/2/library/webbrowser.html")
+    webbrowser.open(url)
     break_count = break_count+1
