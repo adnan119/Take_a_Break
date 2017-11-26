2dbe09c830ef2a9996910bd53a93d6a9d5f8d324
adnan119 <adnanpen119@gmail.com>
2017-11-26 16:35:54 +0530

    show when the program started

--- Take_a_Break.py
+++ Take_a_Break.py
@@ -4,10 +4,10 @@ import time
 
 total_breaks=3
 break_count=0
+time_to_wait=10
 
+print("this program started on " + time.ctime())
 while break_count < total_breaks:
-
-    time_to_wait=10
     time.sleep(time_to_wait)
     webbrowser.open("https://docs.python.org/2/library/webbrowser.html")
     break_count = break_count+1
